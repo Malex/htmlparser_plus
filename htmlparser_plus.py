c91649efe1a6f1e53da4803dbb7c65bdc5f1582e
Malex <malexprojects@gmail.com>
2011-04-02 22:45:10 +0200

    retrieve text finished

--- htmlparser_plus.py
+++ htmlparser_plus.py
@@ -14,11 +14,11 @@ class _Parser(html.parser.HTMLParser):
 		super().feed(self,data)
 
 	@property
-	def tag_start_pos(self) -> dict:
+	def tag_start(self) -> dict:
 		return self._s_pos
 
 	@property
-	def tag_end_pos(self) -> dict:
+	def tag_end(self) -> dict:
 		return self._e_pos
 
 	def handle_starttag(self,tag,attrs):
@@ -30,19 +30,24 @@ class _Parser(html.parser.HTMLParser):
 		self._empties.append(tag)
 
 	def handle_endtag(self,tag):#,attrs):
-		if not tag in self.tag_start_pos.keys() or len(self.tag_start_pos[tag])<=len(self.tag_end_pos[tag]):
+		if not tag in self.tag_start.keys() or len(self.tag_start[tag])<=len(self.tag_end[tag]):
 			raise ParserError("No matching start tag for {}".format(tag))
 		else:
-			if not tag in self.tag_end_pos.keys():
+			if not tag in self.tag_end.keys():
 				self._e_pos[tag]=[]
 			self._e_pos[tag].append(self.getpos())
 
-	def retrieve_text(self,tag): ##TODO: choose specific tag
-		if not tag in self.tag_start_pos.keys():
+	def retrieve_text(self,tag :str) -> str: ##TODO: choose specific tag
+		if not tag in self.tag_start.keys():
 			if tag in self._empties:
 				return ""
 			else:
 				raise ParserError("Tag {} not found".format(tag))
 		else:
-			t = self._store.split('\n')
-			##TODO: to finish
+			lines = self._store.split('\n')
+			st = self.tag_start[tag]
+			end = self.tag_end[tag]
+			sub = [st[0]:end[0]+1]
+			sub[0] = sub[0][st[1]:]
+			sub[-1] = sub[-1][:end[1]]
+			return '\n'.join(sub)
